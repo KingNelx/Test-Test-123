207d565cbd1a490631e40b613e9dd961698a6a91
KingNelx <tapiajonel606@gmail.com>
2022-10-25 08:56:09 +0800

    set up environment

---
+++ src/test/java/crudbe/CrudBeApplicationTests.java
@@ -0,0 +1,13 @@
+package crudbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
