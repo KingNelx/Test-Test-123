db850417feaca49cf936827473946ce7191aff3a
KingNelx <tapiajonel606@gmail.com>
2022-10-25 09:16:44 +0800

    added mvc

---
+++ src/main/java/crudbe/Entity/User.java
@@ -0,0 +1,15 @@
+package crudbe.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+
+
+public class User {
+}
